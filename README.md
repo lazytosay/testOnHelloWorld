eaaa7a05578d4503b87876e97dac2ba6ebd8ddc6
lazytosay <siweisijiao.weebly@gmail.com>
2020-02-10 16:15:14 -0800

    my third commit..
    this time, change 2 files instead of 1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # testOnHelloWorld
  just for github workshop
+ this is a test for the github workshop examples
--- helloWorld.py
+++ helloWorld.py
@@ -1,6 +1,10 @@
 print("this is hello world..")
 print("this is another hello....from")
 
+#I didnt change anything.
 for i in range(100):
     print("the current i is: " , i)
+
+for j in range(2000):
+    print("the current j: " ,j)
 
